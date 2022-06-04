d9734adccf154e3d2af4eb6c21c4c890d1714478
Mikko Malm <=>
2022-06-04 20:07:21 +0300

    4th done!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+
+    for (let i = 0; i < array.length; i++) {
+        
+        if (array[i]==remove) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+
+    return array;
 
 };
 
