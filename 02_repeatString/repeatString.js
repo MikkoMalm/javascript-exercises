4a82ccebdebf3d54cd8d49bf870e33da5fa952c1
Mikko Malm <=>
2022-06-04 16:50:08 +0300

    Made functions more readable.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,11 @@
 const repeatString = function(string, number) {
+    
     newString = "";
 
     for (let i = 0; i < number; i++) {
         newString+=string;
     }
-    
+
     return newString;
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,9 @@
 const reverseString = function(string) {
+    
     let splitString = string.split("");
     let reverseArray = splitString.reverse();
     let joinString = reverseArray.join("")
-    console.log(joinString);
+
     return joinString
 };
 
