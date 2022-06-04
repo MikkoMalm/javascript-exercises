0ced7b8780573a5129079ca7dddcdaea66f18522
Mikko Malm <=>
2022-06-04 23:19:03 +0300

    7th done! toFixed() returns a string can you imagine..

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const ftoc = function() {
+const ftoc = function(number) {
+
+  let conversion = (number - 32) / 1.8000;
+
+  return parseInt(conversion.toFixed(1));
 
 };
 
-const ctof = function() {
+const ctof = function(number) {
+
+  let conversion = number * 1.8000 + 32;
+  
+  return parseInt(conversion.toFixed(1));
 
 };
 
