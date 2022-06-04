06d5db147fcdc3fbe45fbafdd69152c905736ac3
Mikko Malm <=>
2022-06-04 16:42:08 +0300

    3rd done!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let joinString = reverseArray.join("")
+    console.log(joinString);
+    return joinString
 };
 
 // Do not edit below this line
