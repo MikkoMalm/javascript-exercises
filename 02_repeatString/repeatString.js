e8e8257c3c1014a3b436255251d2f4dd8fdfec04
Mikko Malm <=>
2022-06-04 16:27:11 +0300

    2nd quest done!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    newString = "";
 
+    for (let i = 0; i < number; i++) {
+        newString+=string;
+    }
+    
+    return newString;
 };
 
 // Do not edit below this line
